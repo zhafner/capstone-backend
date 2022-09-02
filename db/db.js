a6500d406efc5daea1226476b5d17f36ef94ca12
zhafner <104408611+zhafner@users.noreply.github.com>
2022-09-01 20:13:05 -0400

    Fix DB URL for deploy

--- db/db.js
+++ db/db.js
@@ -1,6 +1,29 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://zacharyhafner@localhost:5432/users");
+let databaseURL = process.env.DATABASE_URL;
+
+// if running database on localhost
+if (!databaseURL) {
+    // set databaseURL to the localhost database URL
+    databaseURL = "postgres://zacharyhafner@localhost:5432/users";
+    // set options for database running on localhost
+    options = {
+        // set logging to false
+        logging: false,
+    };
+} else {
+    // set options for database running on heroku
+    options.dialectOptions = {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    };
+}
+// initialize an instance of Sequelize to connect to the database
+// passing the URI options as arguments
+const db = new Sequelize(databaseURL, options);
+
 const User = require("./User")(db);
 
 const connectToDB = async ()=>{
